66fbf8828cdbf9be1c72b74b5350a47d66868fb3
Gabrielam0305 <gabrielamontes0305@gmail.com>
2024-06-16 20:03:30 -0600

    login

--- Frontend/proyecto2/src/App.js
+++ Frontend/proyecto2/src/App.js
@@ -3,9 +3,20 @@ import "./App.css";
 import Login from "./Screen/login/login";
 import Info from "./Screen/Info/Info";
 import Lista from "./Lista";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="general">{/* <Login /> */ <Lista />}</div>;
+  return (
+    <Router>
+      <div className="general">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="/lista" element={<Lista />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- Frontend/proyecto2/src/Screen/login/login.js
+++ Frontend/proyecto2/src/Screen/login/login.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./login.css"; // Importa un archivo CSS para estilos específicos
 
 function Login() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Aquí puedes realizar la lógica de autenticación si es necesario
+
+    // Redirigir a la ruta /info
+    navigate("/info");
+  };
+
   return (
     <div className="general">
       <h1>Login</h1>
@@ -13,7 +23,9 @@ function Login() {
         <label htmlFor="password">Password</label>
         <input type="password" id="password" name="password" />
       </div>
-      <button type="submit">Login</button>
+      <button type="submit" onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 }
