a08f5595814eb41ddcca6cdd1ed1c28bc632bf98
Gabrielam0305 <gabrielamontes0305@gmail.com>
2024-06-16 20:48:52 -0600

    conexion

--- Backend/index.js
+++ Backend/index.js
@@ -1,44 +1,58 @@
 const express = require("express");
+const { Pool } = require("pg");
 const sql = require("mssql");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+app.options("*", cors());
 const port = 3000;
 
-const dbConfig = {
+// Configuración para PostgreSQL
+const pgPool = new Pool({
+  user: "postgres",
+  password: "lamejorcontra",
+  host: "postgres.c4f8oexlglzn.us-east-1.rds.amazonaws.com",
+  database: "postgresDatabase",
+  port: 5432, // Puerto por defecto de PostgreSQL
+  ssl: {
+    rejectUnauthorized: false, // Necesario si usas SSL en tu base de datos PostgreSQL
+  },
+});
+
+// Configuración para SQL Server
+const sqlConfig = {
   user: "admin",
   password: "lamejorcontra",
   server: "sqlserver.c4f8oexlglzn.us-east-1.rds.amazonaws.com",
-  database: "nombre_de_tu_base_de_datos",
+  database: "sqlserver",
   options: {
-    encrypt: true, // Usar en Azure
-    trustServerCertificate: false, // Cambiar a true para un entorno de desarrollo
+    encrypt: true,
+    trustServerCertificate: true,
   },
   port: 1433,
 };
 
+// Conexión a PostgreSQL
+pgPool.connect((err, client, done) => {
+  if (err) {
+    console.error("Error de conexión a PostgreSQL:", err);
+    return;
+  }
+  console.log("Conexión a PostgreSQL exitosa");
+});
+
+// Conexión a SQL Server
 sql
-  .connect(dbConfig)
+  .connect(sqlConfig)
   .then((pool) => {
     if (pool.connected) {
-      console.log("Conexión a la base de datos exitosa");
+      console.log("Conexión a SQL Server exitosa");
     }
     return pool;
   })
-  .catch((err) => console.error("Error de conexión a la base de datos:", err));
+  .catch((err) => console.error("Error de conexión a SQL Server:", err));
 
-app.get("/api/data", async (req, res) => {
-  try {
-    const pool = await sql.connect(dbConfig);
-    const result = await pool.request().query("SELECT * FROM tu_tabla");
-    res.json(result.recordset);
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
-});
-
-const bodyParser = require("body-parser");
-var urlencodeparser = bodyParser.urlencoded({ extended: true });
-app.use(urlencodeparser);
 app.listen(port, () => {
-  console.log("corriendo en ", port);
+  console.log("Servidor corriendo en el puerto:", port);
 });
